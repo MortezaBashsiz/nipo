21f5bad248f1bd7430907a24b348750ce307c520
bashsiz <morteza.bashsiz@gmail.com>
2021-02-11 10:08:55 +0330

    multiple clients support added

--- nipo/socket.go
+++ nipo/socket.go
@@ -8,15 +8,8 @@ import (
 	"strings"
 )
 
-func (database *Database) OpenSocket(config *Config) {
-	config.logger("Opennig Socket on "+config.Listen.Ip+":"+config.Listen.Port+"/"+config.Listen.Protocol)
-	socket,err := net.Listen(config.Listen.Protocol, config.Listen.Ip+":"+config.Listen.Port)
-	if err != nil {
-        config.logger("Error listening: "+err.Error())
-		os.Exit(1)
-    }
-	defer socket.Close()
-	connection, err := socket.Accept()
+func (database *Database) HandelSocket(connection net.Conn) {
+	defer connection.Close()
 	for {
 		connection.Write([]byte("nipo > "))
 		input, err := bufio.NewReader(connection).ReadString('\n')
@@ -32,5 +25,22 @@ func (database *Database) OpenSocket(config *Config) {
         returneddb.Foreach(func (key,value string) {
             connection.Write([]byte("> "+key+" "+value))
 		})
+	}
+}
+
+func (database *Database) OpenSocket(config *Config) {
+	config.logger("Opennig Socket on "+config.Listen.Ip+":"+config.Listen.Port+"/"+config.Listen.Protocol)
+	socket,err := net.Listen(config.Listen.Protocol, config.Listen.Ip+":"+config.Listen.Port)
+	if err != nil {
+        config.logger("Error listening: "+err.Error())
+		os.Exit(1)
+	}
+	defer socket.Close()
+	for {
+		connection, err := socket.Accept()
+		if err != nil {
+			config.logger("Error accepting socket: "+err.Error())
+		}
+		go database.HandelSocket(connection)
 	}
 }
