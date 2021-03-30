8379b5c8e1c11e0833d5fd79929c90ea265510be
aliinejad <m.alinejad.h@gmail.com>
2021-03-30 19:07:31 +0430

    bug - delete sock.recieve in connect def

--- nipolib/python/sock.py
+++ nipolib/python/sock.py
@@ -11,18 +11,13 @@ class nipoSocket:
 
     def connect(self, host, port):
         self.sock.connect((host, port))
-        err = self.sock.recv(1024)
-        if err != None :
-            print(repr(err))
 
 
     def send(self, msg):
-        self.sock.send(bytes(msg , 'utf-8') )
-        err = self.sock.recv(1024)
-        if err != None :
-            print(repr(err))
-
-
+        self.sock.sendall(bytes(msg , 'utf-8') )
+        response = self.sock.recv(1024)
+        if response != None :
+            print (repr(response))
 
 
 
