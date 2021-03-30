f29990b0d7c880a01d55c2db2192c8200fab2064
javad <Ja7adr@gmail.com>
2021-03-30 20:16:54 +0430

    1. sorted imports 2. removed unwrap parentheses

--- nipo/logger.go
+++ nipo/logger.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-    "log"
-    "os"
-    "fmt"
-    "strings"
+	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 /*
@@ -12,19 +12,19 @@ writes the given log into log file and stdout
 */
 func (config *Config) logger(strLog string, level int) {
 	file, err := os.OpenFile(config.Log.Path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-    prefix := ""
-    if err != nil {
-        log.Fatal(err)
-    }
-    log.SetOutput(file)
-    if config.Log.Level >= level {
-        if level == 1 {
-            prefix = "INFO "
-        }
-        if level == 2 {
-            prefix = "DEBUG "
-        }
-        fmt.Println(strings.TrimSuffix((prefix + strLog), "\n"))
-        log.Println(strings.TrimSuffix((prefix + strLog), "\n"))
-    }
+	prefix := ""
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(file)
+	if config.Log.Level >= level {
+		if level == 1 {
+			prefix = "INFO "
+		}
+		if level == 2 {
+			prefix = "DEBUG "
+		}
+		fmt.Println(strings.TrimSuffix(prefix+strLog, "\n"))
+		log.Println(strings.TrimSuffix(prefix+strLog, "\n"))
+	}
 }
